dd07cb9481c3a5b1761cbcc73b5cbdceed4bb9ff
“Wol-3” <oluwoleoguns@gmail.com>
2023-07-19 02:06:36 -0700

    Initial Commit

---
+++ PullOut/PullOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullOutApp.swift
+//  PullOut
+//
+//  Created by wole on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PullOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
